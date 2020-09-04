9550a8ba1c47c3af5862b2e52d391119c55cd505
Ricardo Silva <ricardoafsilva@hotmail.com>
2020-09-04 10:25:27 -0300

    Updating core, including cache

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,19 +1,42 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
+import localforage from 'localforage'
+import memoryDriver from 'localforage-memoryStorageDriver'
+import { setup } from 'axios-cache-adapter'
 
 export default {
-    async getData(selectedRoom: string, selectedStyle: string): Promise<ResultItemInterface> {
-        return axios({
-            'method': 'GET',
-            'url': 'https://www.googleapis.com/customsearch/v1',
-            'headers': {},
-            'params': {
-                'key': process.env.INPIRANCE_KEY,
-                'cx': process.env.CUSTOM_SEARCH_KEY,
-                'q': `${selectedRoom}+${selectedStyle}+decor`,
-                'searchType': 'image',
-            },
-        }).then(response => {
-            return response.data.items
+    getData(selectedRoom: string, selectedStyle: string): Promise<void | ResultItemInterface> {
+        async function configure(): Promise<AxiosInstance> {
+            await localforage.defineDriver(memoryDriver)
+
+            const store = localforage.createInstance({
+                driver: [
+                    localforage.INDEXEDDB,
+                    localforage.LOCALSTORAGE,
+                    memoryDriver._driver
+                ],
+                name: 'inspirance-cache'
+            })
+
+            return setup({
+                baseURL: 'https://www.googleapis.com/customsearch/v1',
+                method: 'GET',
+                params: {
+                    key: process.env.INPIRANCE_KEY,
+                    cx: process.env.CUSTOM_SEARCH_KEY,
+                    q: `${selectedRoom}+${selectedStyle}+decor`,
+                    searchType: 'image',
+                },
+                cache: {
+                    maxAge: 15 * 60 * 1000,
+                    store
+                }
+            })
+        }
+
+        return configure().then(async (api) => {
+            return await api.get('').then(response => {
+                return response.data.items
+            })
         })
     }
 }
